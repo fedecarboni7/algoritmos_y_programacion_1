759fb7e20113843407777eb8e5849ff76b73c804
fedecarboni7 <fedecarboni7@gmail.com>
2020-10-14 10:50:47 -0300

    ejercicios funciones

--- fiuba/algoritmos y programacion I/clases/ej1_funciones.py
+++ fiuba/algoritmos y programacion I/clases/ej1_funciones.py
@@ -1,17 +1,22 @@
-"""Solicitar el ingreso de un número, calcular su factorial y luego informarlo. 
+"""Solicitar el ingreso de un número, calcular su factorial y luego informarlo.
 Tener en cuenta que el factorial de un número N, entero positivo, es el producto de todos los números enteros
-positivos desde 1 hasta N; y que el factorial de 0, es 1. Si el usuario ingresa un valor menor a cero, se debe 
+positivos desde 1 hasta N; y que el factorial de 0, es 1. Si el usuario ingresa un valor menor a cero, se debe
 mostrar un mensaje que diga que no es factible calcular el factorial de dicho número."""
-def factorial (num):
-        fact = 1
-        for i in range(1,num+1):
-            fact *= i
-        return fact
-num = int(input("Ingresar numero:"))
-while(num < 0):
-    print("No se permiten numeros negativos")
+
+
+def ingresar_num():
     num = int(input("Ingresar numero:"))
-if(num == 0):
-    print("El factorial del numero ingresado es: 1")
-else:
-    print("El factorial del numero ingresado es:", factorial(num))
+    while(num < 0):
+        print("No se permiten numeros negativos")
+        num = int(input("Ingresar numero:"))
+    return num
+
+
+def factorial(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact *= i
+    return fact
+
+
+print("El factorial del numero ingresado es:", factorial(ingresar_num()))
--- fiuba/algoritmos y programacion I/clases/ej2_funciones.py
+++ fiuba/algoritmos y programacion I/clases/ej2_funciones.py
@@ -1,9 +1,17 @@
-"""Escribir un programa modular, que solicite el ingreso de un valor, e informe si se trata de un número primo."""
-num = 991
-for i in range(2, num):
-  if (num%i == 0):
-    break
-if (i+1 == num):
-  print("es primo")
+# Escribir un programa modular, que solicite el ingreso de un valor, e informe si se trata de un número primo.
+
+def num_primo(num):
+    if(num == 1):
+        return True
+    else:
+        for i in range(2, num):
+            if (num % i == 0):
+                break
+        return(i+1 == num)
+
+
+num = int(input("Ingresar numero natural: "))
+if num_primo(num):
+    print("Es primo")
 else:
- print("no es primo")
+    print("No es primo")
