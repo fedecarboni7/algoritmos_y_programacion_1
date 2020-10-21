fa589a310abd093a602ae4434357185c512030ed
fedecarboni7 <fedecarboni7@gmail.com>
2020-10-21 20:27:55 -0300

    rpl 7 and 8

--- fiuba/algoritmos y programacion I/rpl.codes/Estructuras_Básicas/7 - Calculando potencias.py
+++ fiuba/algoritmos y programacion I/rpl.codes/Estructuras_Básicas/7 - Calculando potencias.py
@@ -1,6 +1,6 @@
-resultado = 0
+resultado = 1
 base = int(input("Ingrese la base: "))
 potencia = int(input("Ingrese la potencia: "))
-for i in range(0,potencia):
-    resultado += base * base
+for i in range(potencia):
+    resultado *= base
 print(resultado)
