4d0f76bced2ebee52ebb5f8cbfdf0db356eaa9b6
fedecarboni7 <fedecarboni7@gmail.com>
2020-11-05 17:54:49 -0300

    intento de ejercicio 3.5 rpl

--- fiuba/algoritmos y programacion I/rpl.codes/Strings/5 - Separadores.py
+++ fiuba/algoritmos y programacion I/rpl.codes/Strings/5 - Separadores.py
@@ -1,3 +1,8 @@
 def insertar_separadores(cadena, separador, espaciado):
-    
-    return
+    cadena_final = cadena.split()
+    for i in range(espaciado, len(cadena) + 1, espaciado):
+        cadena_final.insert(i, separador)
+
+    return print(str(cadena_final))
+
+insertar_separadores("255255255255", ".", 3)
