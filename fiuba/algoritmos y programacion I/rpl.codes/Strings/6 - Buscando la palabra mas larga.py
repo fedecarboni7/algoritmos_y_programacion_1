d877c88a2ade8aaa8fc46632cec64023cb51ba4a
fedecarboni7 <fedecarboni7@gmail.com>
2020-11-07 20:08:25 -0300

    rpl strings 6

---
+++ fiuba/algoritmos y programacion I/rpl.codes/Strings/6 - Buscando la palabra mas larga.py
@@ -0,0 +1,13 @@
+def palabra_mas_larga2(texto):
+    palabra_larga = ""
+    inicio_palabra = 0
+    for i in range(0, len(texto)):
+        if texto[i] == " ":
+            palabra_actual = texto[inicio_palabra:i]
+            if len(palabra_larga) < len(palabra_actual):
+                palabra_larga = palabra_actual
+            inicio_palabra = i + 1
+    palabra_actual = texto[inicio_palabra:]
+    if len(palabra_larga) < len(palabra_actual):
+        palabra_larga = palabra_actual
+    return palabra_larga
