dd22e7d061f9f9ec74b8f61fe1bdeff0ed8553de
fedecarboni7 <fedecarboni7@gmail.com>
2020-11-11 23:34:08 -0300

    rpl listas 1

---
+++ fiuba/algoritmos y programacion I/rpl.codes/4- Listas/1 - Iterando.py
@@ -0,0 +1,22 @@
+def filtrar_pares(lista):
+    pass
+
+
+def filtrar_primos(lista):
+    pass
+
+
+def sumar_elementos(lista):
+    pass
+
+
+def esta_ordenada(lista):
+    pass
+
+
+def producto_escalar(vector_1, vector_2):
+    pass
+
+
+def letras_en_palabra(letras, frase):
+    pass
