a33212c1ac7da8478b74a1b0c5c8e89c38324da4
fedecarboni7 <fedecarboni7@gmail.com>
2020-12-04 00:00:32 -0300

    clase 03-12

---
+++ fiuba/algoritmos y programacion I/clases/diciembre_2020/03-12/ejercicio1.py
@@ -0,0 +1,63 @@
+def leer_registro(fh):
+    
+    linea = fh.readline()
+    
+    if linea:
+        registro = linea.rstrip('\n').split(',')
+    else:
+        registro = '99999','','',''
+        
+    return registro
+
+def merge_inscripciones():
+    
+    inscr_web_fh     = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\inscripciones_web.csv','r')
+    inscr_tel_fh     = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\inscripciones_tel.csv','r')
+    inscr_pre_fh     = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\inscripciones_pre.csv','r')
+    inscripciones_fh = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\inscripciones.csv','w')
+    
+    padron_web,materia_web,fecha_web,curso_web = leer_registro(inscr_web_fh)
+    padron_tel,materia_tel,fecha_tel,curso_tel = leer_registro(inscr_tel_fh)
+    padron_pre,materia_pre,fecha_pre,curso_pre = leer_registro(inscr_pre_fh)
+    
+    while padron_web != '99999' or padron_tel != '99999' or padron_pre != '99999':
+        
+        clave_web = padron_web + materia_web + fecha_web
+        clave_tel = padron_tel + materia_tel + fecha_tel
+        clave_pre = padron_pre + materia_pre + fecha_pre
+        
+        menor = min(clave_web,clave_tel,clave_pre)
+        
+        while padron_web != '99999' and clave_web == menor:
+            
+            inscripciones_fh.write('{},{},{},{}\n'.format(padron_web,materia_web,fecha_web,curso_web))
+            
+            padron_web,materia_web,fecha_web,curso_web = leer_registro(inscr_web_fh)
+            
+            clave_web = padron_web + materia_web + fecha_web
+            
+        while padron_tel != '99999' and clave_tel == menor:
+            
+            inscripciones_fh.write('{},{},{},{}\n'.format(padron_tel,materia_tel,fecha_tel,curso_tel))
+            
+            padron_tel,materia_tel,fecha_tel,curso_tel = leer_registro(inscr_tel_fh)
+            
+            clave_tel = padron_tel + materia_tel + fecha_tel
+            
+        while padron_pre != '99999' and clave_pre == menor:
+            
+            inscripciones_fh.write('{},{},{},{}\n'.format(padron_pre,materia_pre,fecha_pre,curso_pre))
+            
+            padron_pre,materia_pre,fecha_pre,curso_pre = leer_registro(inscr_pre_fh)
+            
+            clave_pre = padron_pre + materia_pre + fecha_pre
+            
+   
+    inscr_web_fh.close()
+    inscr_tel_fh.close()
+    inscr_pre_fh.close()
+    inscripciones_fh.close()
+    
+    return
+
+merge_inscripciones()
---
+++ fiuba/algoritmos y programacion I/clases/diciembre_2020/03-12/ejercicio2.py
@@ -0,0 +1,93 @@
+# Se tienen los archivos evaluaciones_web.csv evaluaciones_pre.csv 
+# formato de registro: padron,materia,nota
+# Los archivos están ordenados por padron y materia
+# Se quiere generar un archivo promedios.csv con los promedios ordenado por padron y materia
+#Registro del archivo promedios: padron,materia,promedio
+# c:\users\inés\evaluaciones_web.csv
+# 80295,70.10,7.5
+# 80297,70.10,10
+# 80297,70.50,5
+# 80300,70.10,4
+# 80300,70.10,10
+# 80301,70.10,7
+# 80307,70.10,8
+# c:\users\inés\evaluaciones_pre.csv
+# 80293,70.10,10
+# 80297,70.70,7
+# 80297,70.40,4.5
+# 80300,70.10,8
+# 80300,70.10,9
+# ---------------------------------------------
+# Resultado esperado
+# 80293,70.10,10.0
+# 80295,70.10,7.5
+# 80297,70.10,10.0
+# 80297,70.50,5.0
+# 80297,70.70,7.0
+# 80297,70.40,4.5
+# 80300,70.10,7.75
+# 80301,70.10,7.0
+# 80307,70.10,8.0
+
+def leer_registro(fh):
+    
+    linea = fh.readline()
+    
+    if linea:
+        registro = linea.rstrip('\n').split(',')
+    else:
+        registro = '99999','99.99',''
+        
+    return registro
+
+def generar_archivo_promedios():
+    
+    evaluaciones_web_fh = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\evaluaciones_web.csv','r')
+    evaluaciones_pre_fh = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\evaluaciones_pre.csv','r')
+    promedios_fh        = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\promedios.csv','w')
+    
+    padron_web,materia_web,nota_web = leer_registro(evaluaciones_web_fh)
+    padron_pre,materia_pre,nota_pre = leer_registro(evaluaciones_pre_fh)
+    
+    while padron_web != '99999' or padron_pre != '99999':
+        
+        clave_web = padron_web + materia_web
+        clave_pre = padron_pre + materia_pre
+        
+        menor = min(clave_web,clave_pre)
+        
+        menor_total_notas = 0.0
+        menor_cant_notas  = 0
+        
+        while padron_web != '99999' and clave_web == menor:
+            
+            menor_total_notas += float(nota_web)
+            menor_cant_notas  += 1
+            
+            padron_web,materia_web,nota_web = leer_registro(evaluaciones_web_fh)
+            
+            clave_web = padron_web + materia_web
+            
+        while padron_pre != '99999' and clave_pre == menor:
+            
+            menor_total_notas += float(nota_pre)
+            menor_cant_notas  += 1
+            
+            padron_pre,materia_pre,nota_pre = leer_registro(evaluaciones_pre_fh)
+            
+            clave_pre = padron_pre + materia_pre
+        
+        promedio = menor_total_notas / menor_cant_notas
+        
+        menor_padron  = menor[0:4]
+        menor_materia = menor[4:9]
+            
+        promedios_fh.write('{},{},{}\n'.format(menor_padron,menor_materia,promedio))
+    
+    evaluaciones_web_fh.close()
+    evaluaciones_pre_fh.close()
+    promedios_fh.close()
+    
+    return
+
+generar_archivo_promedios()
