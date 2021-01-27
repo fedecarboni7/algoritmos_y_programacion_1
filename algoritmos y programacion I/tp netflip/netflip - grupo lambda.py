c71fbede4caaa2dfa85ebffe6e26e47707debfac
fedecarboni7 <fedecarboni7@gmail.com>
2021-01-26 23:15:32 -0300

    tp netflip submenu usuarios

--- algoritmos y programacion I/tp netflip/netflip - grupo lambda.py
+++ algoritmos y programacion I/tp netflip/netflip - grupo lambda.py
@@ -24,8 +24,61 @@ def menu(opcion):
         menu(mostrar_menu())
     return
 
+def leer_info(usuario):
+    linea = usuario.readline()
+    if linea:
+        registro = linea.rstrip('\n').split(',')
+    else:
+        registro = ['999999'] # Condición de salida del while
+    return registro
+
 def usuarios():
-    return None
+    try:
+        usuarios_1 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_1.csv','r')
+    except FileNotFoundError:
+        usuarios_1 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_1.csv','w')
+    try:
+        usuarios_2 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_2.csv','r')
+    except FileNotFoundError:
+        usuarios_2 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_2.csv','w')
+    try:
+        usuarios_3 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_3.csv','r')
+    except FileNotFoundError:
+        usuarios_3 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_3.csv','w')
+    
+    usuarios_merge = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_merge.csv','w')
+
+    csv_error = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\log.csv','w')
+
+    clave_usuario_1 = id_usuario_1, nombre_apellido_1, año_de_nacimiento_1, lista_peliculas_1 = leer_info(usuarios_1)
+    
+    clave_usuario_2 = id_usuario_2, nombre_apellido_2, año_de_nacimiento_2, lista_peliculas_2 = leer_info(usuarios_2)
+
+    clave_usuario_3 = id_usuario_3, nombre_apellido_3, año_de_nacimiento_3, lista_peliculas_3 = leer_info(usuarios_3)
+
+    max = "999999"
+    clave_anterior = [" "," "," "," "]
+
+    while id_usuario_1 != max or id_usuario_2 != max or id_usuario_3 != max:
+        
+        men = min(clave_usuario_1, clave_usuario_2, clave_usuario_3)
+        
+        while men[0] == clave_anterior[0]:
+            if men[1] != clave_anterior[1] or men[2] != clave_anterior[2]:
+                csv_error.write(f"Las claves {men} y {clave_anterior} no coinciden\n")
+            else:
+                usuarios_merge.write(clave_anterior[0], clave_anterior[1], clave_anterior[2], clave_anterior[3] + men[3]) ##en caso de tirar error, hacer primero el formato en una variable, y despues pasarla al write
+
+            clave_anterior = men
+
+            if men = clave_usuario_1:
+                clave_usuario_1 = id_usuario_1, nombre_apellido_1, año_de_nacimiento_1, lista_peliculas_1 = leer_info(usuarios_1)
+            elif men = clave_usuario_2:
+                clave_usuario_2 = id_usuario_2, nombre_apellido_2, año_de_nacimiento_2, lista_peliculas_2 = leer_info(usuarios_2)
+            elif men = clave_usuario_3:
+                clave_usuario_3 = id_usuario_3, nombre_apellido_3, año_de_nacimiento_3, lista_peliculas_3 = leer_info(usuarios_3)
+
+    return
 
 def peliculas():
     return None
