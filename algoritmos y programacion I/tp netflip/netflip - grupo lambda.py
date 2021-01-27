3baea31a12cd03d9904c498add0e2142238179f2
fedecarboni7 <fedecarboni7@gmail.com>
2021-01-27 15:51:28 -0300

    "funcion" de ruta, carpeta de archivos

--- algoritmos y programacion I/tp netflip/netflip - grupo lambda.py
+++ algoritmos y programacion I/tp netflip/netflip - grupo lambda.py
@@ -33,22 +33,25 @@ def leer_info(usuario):
     return registro
 
 def usuarios():
-    try:
-        usuarios_1 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_1.csv','r')
-    except FileNotFoundError:
-        usuarios_1 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_1.csv','w')
-    try:
-        usuarios_2 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_2.csv','r')
-    except FileNotFoundError:
-        usuarios_2 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_2.csv','w')
-    try:
-        usuarios_3 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_3.csv','r')
-    except FileNotFoundError:
-        usuarios_3 = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_3.csv','w')
-    
-    usuarios_merge = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\usuarios_netflip_merge.csv','w')
 
-    csv_error = open('C:\\Users\\feden\\Documents\\Archivos\\UBA\\FIUBA\\Algoritmos y programacion I\\ejercicios\\log.csv','w')
+    ruta = "C:\\Users\\federico.carboni\\Desktop\\FIUBA Repo\\algoritmos y programacion I\\tp netflip\\archivos\\"
+
+    try:
+        usuarios_1 = open(f'{ruta}usuarios_netflip_1.csv','r')
+    except FileNotFoundError:
+        usuarios_1 = open(f'{ruta}usuarios_netflip_1.csv','w')
+    try:
+        usuarios_2 = open(f'{ruta}usuarios_netflip_2.csv','r')
+    except FileNotFoundError:
+        usuarios_2 = open(f'{ruta}usuarios_netflip_2.csv','w')
+    try:
+        usuarios_3 = open(f'{ruta}usuarios_netflip_3.csv','r')
+    except FileNotFoundError:
+        usuarios_3 = open(f'{ruta}usuarios_netflip_3.csv','w')
+    
+    usuarios_merge = open(f'{ruta}usuarios_netflip_merge.csv','w')
+
+    csv_error = open(f'{ruta}log.csv','w')
 
     clave_usuario_1 = id_usuario_1, nombre_apellido_1, año_de_nacimiento_1, lista_peliculas_1 = leer_info(usuarios_1)
     
@@ -65,17 +68,17 @@ def usuarios():
         
         while men[0] == clave_anterior[0]:
             if men[1] != clave_anterior[1] or men[2] != clave_anterior[2]:
-                csv_error.write(f"Las claves {men} y {clave_anterior} no coinciden\n")
+                csv_error.write(f"Las claves {men} y {clave_anterior} no coinciden\n") ##en caso de tirar error, hacer primero el formato en una variable, y despues pasarla al write
             else:
-                usuarios_merge.write(clave_anterior[0], clave_anterior[1], clave_anterior[2], clave_anterior[3] + men[3]) ##en caso de tirar error, hacer primero el formato en una variable, y despues pasarla al write
+                usuarios_merge.write(clave_anterior[0], clave_anterior[1], clave_anterior[2], clave_anterior[3] + men[3])
 
             clave_anterior = men
 
-            if men = clave_usuario_1:
+            if men == clave_usuario_1:
                 clave_usuario_1 = id_usuario_1, nombre_apellido_1, año_de_nacimiento_1, lista_peliculas_1 = leer_info(usuarios_1)
-            elif men = clave_usuario_2:
+            elif men == clave_usuario_2:
                 clave_usuario_2 = id_usuario_2, nombre_apellido_2, año_de_nacimiento_2, lista_peliculas_2 = leer_info(usuarios_2)
-            elif men = clave_usuario_3:
+            elif men == clave_usuario_3:
                 clave_usuario_3 = id_usuario_3, nombre_apellido_3, año_de_nacimiento_3, lista_peliculas_3 = leer_info(usuarios_3)
 
     return
