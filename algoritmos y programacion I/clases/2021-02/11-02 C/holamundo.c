dfaf17d91245d3a534cdc3505867bb9a4bd4b021
fedecarboni7 <fedecarboni7@gmail.com>
2021-03-01 18:55:35 -0300

    correcciones archivo c

--- algoritmos y programacion I/clases/2021-02/11-02 C/holamundo.c
+++ algoritmos y programacion I/clases/2021-02/11-02 C/holamundo.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    printf ("Hola Mundo");
+    printf ("Hola Mundo\n");
+    printf ("Como andas?");
     return 0;
 }
+
+//Para compilar y depurar, primero hay que ir a Terminal y poner Run Build Task o Crtl + Shift + B
+//Y una vez compilado y creado el archivo.exe, procedemos a abrir una terminal poniendo el path del archivo
+//seguido de .\archivo
